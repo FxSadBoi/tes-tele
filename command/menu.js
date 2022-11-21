efe8401c168c4f8bd111e13bd135c3bc9869a1c1
FxSadBoi <118036662+FxSadBoi@users.noreply.github.com>
2022-11-21 10:58:31 +0700

    Add files via upload

---
+++ command/menu.js
@@ -0,0 +1,23 @@
+exports.menu = (owner, prefix) => {
+  return `──「 ALL MENU 」──
+
+</ Downloader Menu >
+${prefix}tiktok
+${prefix}play
+${prefix}pindl
+${prefix}twitter
+${prefix}youtube
+${prefix}mediafire
+${prefix}instagram
+${prefix}soundcloud
+
+────────────────────
+Made with ❤️ from @${owner}
+────────────────────`
+}
+
+exports.start = (nama,nameBot) => {
+	return `Hello ${nama}👋
+
+I am ${nameBot}, Click AllMenu to display all available menu !`
+	}
